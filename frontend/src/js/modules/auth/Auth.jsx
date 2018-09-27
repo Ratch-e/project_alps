7347852c02d93cfc75c13e1000ba57a57fb3abdc
Viktor Gunin <viktor_gunin@epam.com>
2018-09-27 20:42:33 +0300

    feature(Auth): basic auth front

--- frontend/src/js/modules/auth/Auth.jsx
+++ frontend/src/js/modules/auth/Auth.jsx
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import './style/Auth.css';
 
 export default class Auth extends Component {
+  state = {
+    logged: false,
+  }
+
+  toggleLogin= () => {
+    this.setState({
+      logged: !this.state.logged
+    })
+  }
+
   render() {
+    const { logged } = this.state;
     return (
       <div className="Auth">
         <input className="Auth__input" type="text" />
         <input className="Auth__input" type="password" />
-        <button className="Auth__button">Войти</button>
+
+        <button 
+          onClick={() => this.toggleLogin()}
+          className="Auth__button"
+          >
+          {logged ? 'Выйти' : 'Войти'}
+        </button>
+
+        <a href="#">Зарегистрироваться</a>
+
       </div>
     );
   }
