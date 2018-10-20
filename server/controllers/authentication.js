8091f5f414da64a3d1efe2d6d006b6857a180b4f
Viktor Gunin <viktor_gunin@epam.com>
2018-10-20 18:20:21 +0300

    refactor(auth): improved comments

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,20 +1,34 @@
 const User = require('../models/user')
 
+/**
+ * Checks if an email and a password exist
+ * 
+ * @param {*} email 
+ * @param {*} password 
+ * @param {*} res 
+ */
 const checkCredentials = (email, password, res) => {
   if(!email || !password) {
     return res.status(422).send({error: 'You must provide an email and a password'});
   }
 };
 
+/**
+ * Handles signup request
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 exports.signup = (req, res, next) => {
-  //get password and email
+  //gets a password and an email strings
   const email = req.body.email;
   const password = req.body.password;
 
-  //chack if they are good
+  //checks if they are good
   checkCredentials(email, password, res);
 
-  //check if exist - reject, if not - save
+  //checks if user already exist - reject, if not - save
   User.findOne({ email: email }, (err, existingUser) => {
     if(err) { 
       return next(err) 
@@ -24,8 +38,8 @@ exports.signup = (req, res, next) => {
       return res.status(422).send({ error: 'Email is in use'});
     }
 
-    const user = new User({ email, password });
-
-    user.save(err => err ? next(err) : res.json(user));
+    //if everything is ok - create a new user and save it
+    const newUser = new User({ email, password });
+    newUser.save(err => err ? next(err) : res.json(newUser));
   });
 };
