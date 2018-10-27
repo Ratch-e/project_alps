cde2fc73699253bf4a35d9aa3177e0c24fed5b39
Viktor Gunin <viktor_gunin@epam.com>
2018-10-27 19:32:03 +0300

    refactor: restructured front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Auth from './js/modules/auth/Auth';
+import Auth from './js/components/auth/Auth';
 import './style/index.css';
 
 ReactDOM.render(<Auth />, document.getElementById('root'));
