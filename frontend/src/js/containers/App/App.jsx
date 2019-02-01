a1abd8f56b2b320d24798c6c025b52ed80996b05
ratch-e <guninvK@gmail.com>
2019-02-01 20:21:25 +0400

    fearure(redux): redux store added

--- frontend/src/js/containers/App/App.jsx
+++ frontend/src/js/containers/App/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
 import Routing from '../../containers/Routing/Routing';
+import configureStore from '../../store/configureStore';
+
+const store = configureStore();
 
 export default class App extends Component {
   render() {
-    return(
-      <Routing />
-    ); 
+    return (
+      <Provider store={store}>
+        <Routing />
+      </Provider>
+    );
   }
 }
