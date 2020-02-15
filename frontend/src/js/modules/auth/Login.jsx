bc18ebab5075bb70dfc8f5a2343ed09519fb833a
Viktor Gunin <guninvk@gmail.com>
2020-02-15 17:38:54 +0400

    formInput added

--- frontend/src/js/modules/auth/Login.jsx
+++ frontend/src/js/modules/auth/Login.jsx
@@ -1,86 +1,66 @@
-import React, { useContext, useState } from "react";
-import Axios from "axios";
-import { useHistory } from "react-router-dom";
-import { ErrorMessage, useFormik } from "formik";
+import React, { useContext, useState } from 'react';
+import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { Formik, Form } from 'formik';
 
-import Card from "../../components/Card/Card";
-import Button from "../../components/Button/Button";
-import { SIGNIN_API } from "../../constants/routing";
-import AuthStore from "../../store/AuthStore";
+import Card from '../../components/Card/Card';
+import Button from '../../components/Button/Button';
+import { SIGNIN_API } from '../../constants/routing';
+import AuthStore from '../../store/AuthStore';
+import FormInput from '../../components/Form/FormInput/FormInput';
 
-import "./auth.sass";
+import './auth.sass';
 
 const Login = () => {
-  const history = useHistory();
-  const store = useContext(AuthStore);
-  const [loginError, setLoginError] = useState("");
+    const history = useHistory();
+    const store = useContext(AuthStore);
+    const [loginError, setLoginError] = useState('');
 
-  const validate = values => {
-    let errors = {};
-    if (!values.email) {
-      errors.email = "Необходимо заполнить email";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = "Неправильный формат электронной почты";
-    }
-    return errors;
-  };
+    const validate = (values) => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = 'Необходимо заполнить email';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Неправильный формат электронной почты';
+        }
+        return errors;
+    };
 
-  const authorize = values => {
-    const { email, password } = values;
-    const { setLoggedInUser } = store;
+    const authorize = (values) => {
+        const { email, password } = values;
+        const { setLoggedInUser } = store;
 
-    Axios.post(SIGNIN_API, {
-      email,
-      password,
-    })
-      .then(result => {
-        setLoggedInUser(result.data);
-        localStorage.setItem("AlpsUser", JSON.stringify(result.data));
-        history.push("/");
-      })
-      .catch(() => setLoginError("Email или пароль не верны"));
-  };
+        Axios.post(SIGNIN_API, {
+            email,
+            password,
+        })
+            .then((result) => {
+                setLoggedInUser(result.data);
+                localStorage.setItem('AlpsUser', JSON.stringify(result.data));
+                history.push('/');
+            })
+            .catch(() => setLoginError('Email или пароль не верны'));
+    };
 
-  const formik = useFormik({
-    initialValues: { email: "", password: "" },
-    validate: validate,
-    validateOnChange: false,
-    onSubmit: authorize,
-  });
-
-  return (
-    <Card className="auth">
-      <div className="card__title">Войти</div>
-      <form onSubmit={formik.handleSubmit}>
-        <label className="auth__field">
-          Email
-          <input
-            autoComplete="off"
-            className="auth__input"
-            type="email"
-            name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-        </label>
-        <label className="auth__field">
-          Пароль
-          <input
-            autoComplete="off"
-            className="auth__input"
-            type="password"
-            name="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-        </label>
-        <Button type="submit" disabled={formik.isSubmitting}>
-          Войти
-        </Button>
-        {loginError && <div className="auth__error">{loginError}</div>}
-      </form>
-    </Card>
-  );
+    return (
+        <Card className="auth">
+            <div className="card__title">Войти</div>
+            <Formik initialValues={{ email: '', password: '' }} validate={validate} validateOnChange={false} onSubmit={authorize}>
+                {
+                    ({ isSubmitting }) => (
+                        <Form>
+                            <FormInput name="email" type="email" label="email" />
+                            <FormInput name="password" type="password" label="password" />
+                            <Button type="submit" disabled={isSubmitting}>
+                                Войти
+                            </Button>
+                            {loginError && <div className="auth__error">{loginError}</div>}
+                        </Form>
+                    )
+                }
+            </Formik>
+        </Card>
+    );
 };
 
 export default Login;
--- frontend/src/js/modules/auth/SignUp.jsx
+++ frontend/src/js/modules/auth/SignUp.jsx
@@ -1,110 +1,72 @@
-import React, { useContext, useState } from "react";
-import Axios from "axios";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useHistory } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import Axios from 'axios';
+import { Formik, Form } from 'formik';
+import { useHistory } from 'react-router-dom';
 
-import Card from "../../components/Card/Card";
-import Button from "../../components/Button/Button";
-import { SIGNUP_API } from "../../constants/routing";
-import AuthStore from "../../store/AuthStore";
+import Card from '../../components/Card/Card';
+import Button from '../../components/Button/Button';
+import { SIGNUP_API } from '../../constants/routing';
+import AuthStore from '../../store/AuthStore';
+import FormInput from '../../components/Form/FormInput/FormInput';
 
-import "./auth.sass";
+import './auth.sass';
 
 const SignUp = () => {
-  const history = useHistory();
-  const store = useContext(AuthStore);
-  const [signupError, setSignupError] = useState("");
+    const history = useHistory();
+    const store = useContext(AuthStore);
+    const [signupError, setSignupError] = useState('');
 
-  const authorize = values => {
-    const { email, password } = values;
-    const { setLoggedInUser } = store;
+    const authorize = (values) => {
+        const { email, password } = values;
+        const { setLoggedInUser } = store;
 
-    Axios.post(SIGNUP_API, {
-      email,
-      password
-    })
-      .then(result => {
-        setLoggedInUser(result.data);
-        localStorage.setItem("AlpsToken", JSON.stringify(result.data));
-        history.push("/");
-      })
-      .catch(() => setSignupError("Такой пользователь уже существует"));
-  };
+        Axios.post(SIGNUP_API, {
+            email,
+            password,
+        })
+            .then((result) => {
+                setLoggedInUser(result.data);
+                localStorage.setItem('AlpsToken', JSON.stringify(result.data));
+                history.push('/');
+            })
+            .catch(() => setSignupError('Такой пользователь уже существует'));
+    };
 
-  const validate = values => {
-    let errors = {};
-    if (!values.email) {
-      errors.email = "This field is required";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = "Invalid email address format";
-    } else if (values.password !== values.password2) {
-      errors.password = "Пароли не совпадают";
-    }
-    return errors;
-  };
+    const validate = (values) => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = 'This field is required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Invalid email address format';
+        } else if (values.password !== values.password2) {
+            errors.password = 'Пароли не совпадают';
+        }
+        return errors;
+    };
 
-  return (
-    <Card className="auth">
-      <div className="card__title">Регистрация</div>
-      <Formik
-        initialValues={{ email: "", password: "", password2: "" }}
-        validate={validate}
-        validateOnChange={false}
-        onSubmit={authorize}
-      >
-        {({ handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit}>
-            <label className="auth__field">
-              Email
-              <Field
-                autoComplete="off"
-                className="auth__input"
-                type="email"
-                name="email"
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className="auth__error"
-              />
-            </label>
-            <label className="auth__field">
-              Пароль
-              <Field
-                autoComplete="off"
-                className="auth__input"
-                type="password"
-                name="password"
-              />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="auth__error"
-              />
-            </label>
-            <label className="auth__field">
-              Повторите пароль
-              <Field
-                autoComplete="off"
-                className="auth__input"
-                type="password"
-                name="password2"
-              />
-              <ErrorMessage
-                name="password2"
-                component="div"
-                className="auth__error"
-              />
-            </label>
-            <Button type="submit" disabled={isSubmitting}>
+    return (
+        <Card className="auth">
+            <div className="card__title">Регистрация</div>
+            <Formik
+                initialValues={{ email: '', password: '', password2: '' }}
+                validate={validate}
+                validateOnChange={false}
+                onSubmit={authorize}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <FormInput name="email" type="email" label="email" />
+                        <FormInput name="password" type="password" label="password" />
+                        <FormInput name="password2" type="password" label="Retype the password" />
+                        <Button type="submit" disabled={isSubmitting}>
               Зарегистрироваться
-            </Button>
-            {signupError && <div className="auth__error">{signupError}</div>}
-          </Form>
-        )}
-      </Formik>
-    </Card>
-  );
+                        </Button>
+                        {signupError && <div className="auth__error">{signupError}</div>}
+                    </Form>
+                )}
+            </Formik>
+        </Card>
+    );
 };
 
 export default SignUp;
